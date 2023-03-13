f074ae233420a321a3c478c4b76f6645052b5891
Sam Kagan <skagan@nrao.edu>
2023-03-13 12:15:29 -0600

    Refactored keybindings.lua to use vim.keymap.set

--- config/lua/keybindings.lua
+++ config/lua/keybindings.lua
@@ -1,80 +1,77 @@
 -- Create custom keybindings
 
 return function()
-  local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-  end
   local opts = {noremap = true, silent = true}
 
   -- Bufferline
   -- These commands will navigate through buffers in order regardless of which mode you are using
   -- e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
-  vim.api.nvim_set_keymap("n", "gl", "<CMD>BufferLineCycleNext<CR>", opts)
-  vim.api.nvim_set_keymap("n", "gh", "<CMD>BufferLineCyclePrev<CR>", opts)
+  vim.keymap.set("n", "gl", "<CMD>BufferLineCycleNext<CR>", opts)
+  vim.keymap.set("n", "gh", "<CMD>BufferLineCyclePrev<CR>", opts)
   -- These commands will move the current buffer backwards or forwards in the bufferline
-  vim.api.nvim_set_keymap("n", "g]", "<CMD>BufferLineMoveNext<CR>", opts)
-  vim.api.nvim_set_keymap("n", "g[", "<CMD>BufferLineMovePrev<CR>", opts)
+  vim.keymap.set("n", "g]", "<CMD>BufferLineMoveNext<CR>", opts)
+  vim.keymap.set("n", "g[", "<CMD>BufferLineMovePrev<CR>", opts)
   -- Pick which buffer to go to next
-  vim.api.nvim_set_keymap("n", "gp", "<CMD>BufferLinePick<CR>", opts)
+  vim.keymap.set("n", "gp", "<CMD>BufferLinePick<CR>", opts)
 
   -- Diagnostic
-  vim.api.nvim_set_keymap("n", t"<leader>dj", "<CMD>lua require'lspsaga.diagnostic':goto_next()<CR>",
+  vim.keymap.set("n", "<leader>dj", function() require'lspsaga.diagnostic':goto_next() end,
     opts
   )
-  vim.api.nvim_set_keymap("n", t"<leader>dk", "<CMD>lua require'lspsaga.diagnostic':goto_prev()<CR>",
+  vim.keymap.set("n", "<leader>dk", function() require'lspsaga.diagnostic':goto_prev() end,
     opts
   )
 
   -- LSP
-  vim.api.nvim_set_keymap("n", t"<leader>dd", "<CMD>Lspsaga goto_definition<CR>",
+  vim.keymap.set("n", "<leader>dd", "<CMD>Lspsaga goto_definition<CR>",
     opts
   )
-  vim.api.nvim_set_keymap("n", t"<leader>di",
+  vim.keymap.set("n", "<leader>di",
     "<CMD>Lspsaga code_action<CR>",
     opts
   )
-  vim.api.nvim_set_keymap("n", t"<leader>dr", "<CMD>Lspsaga rename<CR>",
+  vim.keymap.set("n", "<leader>dr", "<CMD>Lspsaga rename<CR>",
     opts
   )
-  vim.api.nvim_set_keymap("n", t"<leader>dh", "<CMD>Lspsaga hover_doc<CR>",
+  vim.keymap.set("n", "<leader>dh", "<CMD>Lspsaga hover_doc<CR>",
     opts
   )
 
   -- Trouble
-  vim.api.nvim_set_keymap("n", t"<leader>rr", "<CMD>TroubleToggle<CR>", opts)
-  vim.api.nvim_set_keymap("n", t"<leader>rw",
+  vim.keymap.set("n", "<leader>rr", "<CMD>TroubleToggle<CR>", opts)
+  vim.keymap.set("n", "<leader>rw",
     "<CMD>Trouble workspace_diagnostics<CR>",
     opts
   )
-  vim.api.nvim_set_keymap("n", t"<leader>rd",
+  vim.keymap.set("n", "<leader>rd",
     "<CMD>Trouble document_diagnostics<CR>",
     opts
   )
-  vim.api.nvim_set_keymap("n", t"<leader>re",
+  vim.keymap.set("n", "<leader>re",
     "<CMD>Trouble lsp_references<CR>",
     opts
   )
-  vim.api.nvim_set_keymap("n", t"<leader>dt", "<CMD>Trouble lsp_type_definitions<CR>", opts)
+  vim.keymap.set("n", "<leader>di", "<CMD>Trouble lsp_type_definitions<CR>", opts)
 
   -- Gitsigns
-  vim.api.nvim_set_keymap("n", t"<leader>jj", "<CMD>Gitsigns next_hunk<CR>", opts)
-  vim.api.nvim_set_keymap("n", t"<leader>jk", "<CMD>Gitsigns prev_hunk<CR>", opts)
-  vim.api.nvim_set_keymap("n", t"<leader>jp", "<CMD>Gitsigns preview_hunk<CR>", opts)
-  vim.api.nvim_set_keymap("n", t"<leader>jl", "<CMD>Gitsigns toggle_current_line_blame<CR>", opts)
-  vim.api.nvim_set_keymap("n", t"<leader>jr", "<CMD>Gitsigns reset_hunk<CR>", opts)
+  vim.keymap.set("n", "<leader>jj", require"gitsigns.actions".next_hunk, opts)
+  vim.keymap.set("n", "<leader>jk", require"gitsigns.actions".prev_hunk, opts)
+  vim.keymap.set("n", "<leader>jp", require"gitsigns.actions".preview_hunk, opts)
+  vim.keymap.set("n", "<leader>jl", require"gitsigns.actions".toggle_current_line_blame, opts)
+  vim.keymap.set("n", "<leader>jr", require"gitsigns.actions".reset_hunk, opts)
 
   -- Treesitter Playground
-  vim.api.nvim_set_keymap("n", t"<leader>pp", "<CMD>TSPlaygroundToggle<CR>", opts)
+  vim.keymap.set("n", "<leader>pp", "<CMD>TSPlaygroundToggle<CR>", opts)
 
   -- Telescope
-  vim.api.nvim_set_keymap("n", t"<leader>tf", "<CMD>Telescope find_files<CR>", opts)
-  vim.api.nvim_set_keymap("n", t"<leader>td", "<CMD>Telescope current_buffer_fuzzy_find<CR>", opts)
-  vim.api.nvim_set_keymap("n", t"<leader>tw", "<CMD>Telescope live_grep<CR>", opts)
-  vim.api.nvim_set_keymap("v", t"<leader>tw", "<CMD>Telescope grep_string<CR>", opts)
-  vim.api.nvim_set_keymap("n", t"<leader>tp", "<CMD>Telescope yank_history<CR>", opts)
+  vim.keymap.set("n", "<leader>tf", require"telescope.builtin".find_files, opts)
+  vim.keymap.set("n", "<leader>td", require"telescope.builtin".current_buffer_fuzzy_find, opts)
+  vim.keymap.set("n", "<leader>tw", require"telescope.builtin".live_grep, opts)
+  vim.keymap.set("v", "<leader>tw", require"telescope.builtin".grep_string, opts)
+  vim.keymap.set("n", "<leader>tp", "<CMD> Telescope yank_history<CR>", opts)
 
   -- Yanky
-  vim.api.nvim_set_keymap("n", "y", "<Plug>(YankyYank)", opts)
+  vim.keymap.set("n", "y", "<Plug>(YankyYank)", opts)
 
   -- Neogen
   vim.keymap.set("n", "<leader>g", require'neogen'.generate, opts)
