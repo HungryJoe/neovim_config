d17552bd2b04a3fd9145e4ffcc0758e06ef29a6c
Sam Kagan <skagan@nrao.edu>
2023-04-27 17:43:57 -0600

    Updated bufferline config to remove deprecated config option

--- config/lua/bufferline_config.lua
+++ config/lua/bufferline_config.lua
@@ -64,7 +64,17 @@ return function()
     color_icons = true, -- whether or not to add the filetype icon highlights
     show_buffer_icons = true, -- disable filetype icons for buffers
     show_buffer_close_icons = false,
-    show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
+    get_element_icon = function(element)
+      -- element consists of {filetype: string, path: string, extension: string, directory: string}
+      -- This can be used to change how bufferline fetches the icon
+      -- for an element e.g. a buffer or a tab.
+      -- e.g.
+      local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
+      return icon, hl
+      -- or
+      -- local custom_map = {my_thing_ft: {icon = "my_thing_icon", hl}}
+      -- return custom_map[element.filetype]
+    end,
     show_close_icon = false,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
