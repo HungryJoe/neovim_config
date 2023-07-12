f183d32c1566ec885ea09d837e64ddd649236c77
Sam Kagan <skagan@nrao.edu>
2023-07-12 16:15:00 -0600

    Removed extraneous files

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 To install:
 
 1. Sym-link `./config` to `~/.config/nvim`
-2. Sym-link `vimrc` to `~/.vimrc`
--- config/lua/sibling_swap_config.lua
+++
@@ -1,9 +0,0 @@
--- Configure the sibling-swap plugin
--- Docs: https://github.com/Wansmer/sibling-swap.nvim
-
-return function()
-  require('sibling-swap').setup({
-    allow_interline_swaps = true,
-    use_default_keymaps = false,
-  })
-end
